fd12f094bcf469fecea9c2f5abc3ebb6994e8a47
Eric Lin <eric.lin@sjsu.edu>
2021-05-08 23:26:13 -0700

    Added function that grabs all companies in sp400 and sp600 sp400 provides all US medium cap stock sp600 provides all US small cap stock

--- functions.py
+++ functions.py
@@ -1,10 +1,29 @@
 import requests
 import pandas as pd
 
+# Medium cap stocks
 def get_sp500_tickers():
     wiki_table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     df = wiki_table[0]
     tickers = df['Symbol'].values
+    # print(tickers)
+    return tickers
+
+# Medium cap stocks
+def get_sp400_tickers():
+    wiki_table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_400_companies')
+    df = wiki_table[0]
+    # print(df)
+    tickers = df['Ticker symbol'].values
+    # print(tickers)
+    return tickers
+
+# Small cap stocks
+def get_sp600_tickers():
+    wiki_table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_600_companies')
+    df = wiki_table[1]
+    print(df)
+    tickers = df['Ticker symbol'].values
     # print(tickers)
     return tickers
 
@@ -20,6 +39,13 @@ def getSuggestions(amount, strats):
         }}
     return result
 
+def value_selection(amount, stocks):
+    # from the list of stocks, return top three and amount to each stocks
+    # select from large cap stocks and choose one that suffered drop recently or traded sideways for a while
+    sp500 = get_sp500_tickers()
+
+    return
+    
 def grab_all_stocks():
     api_key = '97PWgrqMl_bpqgzX9KGLEJvIaN3pq7yJ'
 
