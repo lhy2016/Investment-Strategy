4e5cbf746a4d487618f88dd5da32944b6728d9d8
lhy2016 <lhy920104@gmail.com>
2021-05-11 16:18:37 -0700

    reconstruct stocks and handler map

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request
-from functions import getSuggestions, grab_all_stocks, get_sp500_tickers, get_sp400_tickers, get_sp600_tickers
+from functions import getSuggestions
 
 app = Flask(__name__)
-
-api_key = '97PWgrqMl_bpqgzX9KGLEJvIaN3pq7yJ'
 
 @app.route('/')
 def hello_world():
@@ -11,15 +9,12 @@ def hello_world():
 
 @app.route('/', methods=['POST']) 
 def query():
-    # ret = grab_all_stocks()
-    ret = get_sp600_tickers()
-    print(ret[:20])
-    # print(ret[100])
-    amount = int(request.form['amount'])
+
+    suggestion = getSuggestions(request.form)
+    
+    viewItems = {}
     strats = request.form['strats'].split(",")
     strat1 = strats[0]
-    suggestion = getSuggestions(amount, strats)
-    viewItems = {}
     viewItems['amount'] = request.form['amount']
     viewItems['strat1'] = strat1
     if (len(strats) == 2):
@@ -29,4 +24,5 @@ def query():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
     
