912d5dd0e0470656626f064cf0100c7cddf7e6f5
lhy2016 <lhy920104@gmail.com>
2021-05-15 02:20:33 -0700

    cache services finished

--- functions.py
+++ functions.py
@@ -2,77 +2,50 @@ import requests
 import pandas as pd
 import yfinance as yf
 import time
+from stockService import stocks
 
 def getSuggestions(input):
     strats = input["strats"].split(",")
     products = input["products"].split(",")
+    result = {}
     for product in products:
         for strat in strats:
             handler = handlerMap[product][strat]
             if handler != None:
-                handler()
-    
-    result={
-        "stock": {
-            "spend" : (int)(input["amount"])/3,
-            "names" : ["Google", "Apple"],
-        }, 
-        "ETF": {
-            "spend" : (int)(input["amount"])/3 * 2,
-            "names" : ["iShares U.S. Medical Devices ETF"]
-        }}
+                temp = handler(input)
+                if len(result) == 0:
+                    result = temp
+                elif temp.keys()[0] not in result:
+                    result.update(temp)
+                else:
+                    curTickers = result[temp.keys()[0]]["names"]
+                    toAddTickers = temp[temp.keys()[0]]["names"]
+                    result[temp.keys()[0]]["names"] = list(set(curTickers + toAddTickers))
+    for key in result:
+        result[key]["spend"] = (int)(input["amount"]) / len(result.keys())
     return result
 
 # Large cap stocks
-def get_sp500_tickers():
-    wiki_table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    df = wiki_table[0]
-    tickers = df['Symbol'].values
-    # print(tickers)
-    return tickers
 
-# Medium cap stocks
-def get_sp400_tickers():
-    wiki_table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_400_companies')
-    df = wiki_table[0]
-    # print(df)
-    tickers = df['Ticker symbol'].values
-    # print(tickers)
-    return tickers
-
-# Small cap stocks
-def get_sp600_tickers():
-    wiki_table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_600_companies')
-    df = wiki_table[1]
-    # print(df)
-    tickers = df['Ticker symbol'].values
-    # print(tickers)
-    return tickers
-
-def grab_all_stocks():
-    ret = {}
-    ret["small"] = get_sp600_tickers()
-    ret["medium"] = get_sp400_tickers()
-    ret["large"] = get_sp500_tickers()
-    return ret
 
 def value_selection(amount, stocks):
     # from the list of stocks, return top three and amount to each stocks
     # select from large cap stocks and choose one that suffered drop recently or traded sideways for a while
-    sp500 = get_sp500_tickers()
+    # sp500 = get_sp500_tickers()
 
     return
 
-stocks = grab_all_stocks()    
 
-def get_growth_stocks():
-    small = stocks['small']
-    tickerList = ""
-    for i in range(1):
-        test = "AMZN"
-        stock = yf.Ticker(test)
-        print(stock.earnings)
-    return None
+def get_growth_stocks(input):
+    result={
+        "stock": {
+            "names" : [],
+        },
+    }
+    temp = list(stocks.keys())
+    temp = temp[:5]
+    result["stock"]["names"] = temp
+    return result
 
 handlerMap = {
     "stock":{
--- stockService.py
+++ stockService.py
@@ -1,28 +1,86 @@
-from functions import grab_all_stocks
 import time
 import datetime as dt
 from pytz import timezone
-import os
+import yfinance as yf
+import pandas as pd
 
-stocks = grab_all_stocks()
-file = None
-while True:
-    
-    ue = timezone('US/Eastern')
-    hh = dt.datetime.now(ue).hour 
-    if hh >= 20 or os.stat('stocksInfo.txt').st_size == 0:
-        yy = dt.datetime.now(ue).year
-        mm = dt.datetime.now(ue).month
-        dd = dt.datetime.now(ue).day
-        todayStr = str(yy) + "-" + str(mm) + "-" + str(dd)
-        
-        file = open('stocksInfo.txt', 'w+')
-        dateStr = file.readline()
-        if not dateStr or dateStr != todayStr:
-            file.write(todayStr)
-            for cap in stocks:
-                stockList = stocks[cap]
-                for ticker in stockList:
-                    file.write("\n"+ticker)
-        file.close()
-    time.sleep(6)
+def get_sp500_tickers():
+    wiki_table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    df = wiki_table[0]
+    tickers = df['Symbol'].values
+    # print(tickers)
+    return tickers
+
+# Medium cap stocks
+def get_sp400_tickers():
+    wiki_table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_400_companies')
+    df = wiki_table[0]
+    # print(df)
+    tickers = df['Ticker symbol'].values
+    # print(tickers)
+    return tickers
+
+# Small cap stocks
+def get_sp600_tickers():
+    wiki_table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_600_companies')
+    df = wiki_table[1]
+    # print(df)
+    tickers = df['Ticker symbol'].values
+    # print(tickers)
+    return tickers
+
+def grab_all_stocks():
+    ret = {}
+    ret["small"] = get_sp600_tickers()
+    ret["medium"] = get_sp400_tickers()
+    ret["large"] = get_sp500_tickers()
+    return ret
+
+stocks = {}
+delisted = ["BRK.B", "MOG.A", "JW.A", "TPRE", "BF.B"]
+
+def update():
+    while True:    
+        ue = timezone('US/Eastern')
+        hh = dt.datetime.now(ue).hour 
+        if hh >= 20 or "date" not in stocks:
+            yy = dt.datetime.now(ue).year
+            mm = dt.datetime.now(ue).month
+            dd = dt.datetime.now(ue).day
+            todayStr = str(yy) + "-" + str(mm) + "-" + str(dd)
+            if "date" not in stocks: 
+                stocks["date"] = todayStr
+                tickers = grab_all_stocks()
+                tickers = [*tickers["small"], *tickers["medium"], *tickers["large"]]
+                tickers = list(set(tickers))
+                tickers = [x for x in tickers if x not in delisted]
+                tickerStr = " ".join(tickers)
+
+
+                data = yf.download(tickerStr, period="1y", interval="1wk",threads=True, group_by='tickers')
+                for ticker in tickers:
+                    stocks[ticker] = {}
+                    updated = data.loc[:, (ticker, "High")].dropna()
+                    if(len(updated) > 0):
+                        
+                        stocks[ticker]["history"] = updated
+                        tickerObj = yf.Ticker(ticker)
+                        stocks[ticker]["quarterly_earnings"] = tickerObj.quarterly_earnings
+                        stocks[ticker]["info"] = tickerObj.info
+                    else:
+                        del stocks[ticker]
+
+                print("initialization finished!")
+            elif stocks["date"] != todayStr:
+                stocks["date"] = todayStr
+                for ticker in stocks:
+                    if ticker != "date":
+                        tickerObj = yf.Ticker(ticker)
+                        updated = tickerObj.history(period="1y", interval="1wk")
+                        if (len(updated) > 0):
+                            stocks[ticker]["history"] = updated
+                            stocks[ticker]["quarterly_earnings"] = tickerObj.quarterly_earnings
+                            stocks[ticker]["info"] = tickerObj.info
+                        else:
+                            del stocks[ticker]
+        time.sleep(3600)
